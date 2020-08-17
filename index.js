ed2a8316aa2c578f8ed609264b3a926a5a98ede4
JulienAssouline <julien1993@hotmail.ca>
2020-08-17 11:17:00 -0400

    a new heroku app

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(
+  cors({
+    origin: "https://azurereactstorage.z9.web.core.windows.net/",
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send({ data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] });
+});
+
+app.listen(PORT, () =>
+  console.log(`server listerning on port http://localhost:${PORT}`)
+);
+
+module.exports = app;
